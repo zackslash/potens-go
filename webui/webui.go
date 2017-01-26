1744aa0b7225c1673392aea069e2df34e51b5f35
Brooke Bryan <brooke@bajb.net>
2017-01-26 15:21:15 +0000

    Platform > Applications

--- webui/webui.go
+++ webui/webui.go
@@ -4,39 +4,39 @@ import (
 	"net/url"
 
 	"github.com/cubex/potens-go/webui/breadcrumb"
-	"github.com/cubex/proto-go/platform"
+	"github.com/cubex/proto-go/applications"
 )
 
 //CreateResponse creates a new initialised response
-func CreateResponse() *platform.HTTPResponse {
-	response := &platform.HTTPResponse{}
-	response.Headers = make(map[string]*platform.HTTPResponse_HTTPHeaderParameter)
+func CreateResponse() *applications.HTTPResponse {
+	response := &applications.HTTPResponse{}
+	response.Headers = make(map[string]*applications.HTTPResponse_HTTPHeaderParameter)
 	return response
 }
 
 //SetBreadcrumb set the breadcrumb on the response
-func SetBreadcrumb(response *platform.HTTPResponse, breadcrumb breadcrumb.Breadcrumb) {
-	response.Headers["x-cube-breadcrumb"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{breadcrumb.Json()}}
+func SetBreadcrumb(response *applications.HTTPResponse, breadcrumb breadcrumb.Breadcrumb) {
+	response.Headers["x-cube-breadcrumb"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{breadcrumb.Json()}}
 }
 
 //SetPageTitle set the page title on the response
-func SetPageTitle(response *platform.HTTPResponse, PageTitle string) {
-	response.Headers["x-cube-title"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{PageTitle}}
+func SetPageTitle(response *applications.HTTPResponse, PageTitle string) {
+	response.Headers["x-cube-title"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{PageTitle}}
 }
 
 //SetBackPath set the back path on the response, relative to app route
-func SetBackPath(response *platform.HTTPResponse, BackPath string) {
-	response.Headers["x-cube-back-path"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{BackPath}}
+func SetBackPath(response *applications.HTTPResponse, BackPath string) {
+	response.Headers["x-cube-back-path"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{BackPath}}
 }
 
 //SetPageIcon set the icon url/code on the response
-func SetPageIcon(response *platform.HTTPResponse, Icon string) {
-	response.Headers["x-cube-icon"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{Icon}}
+func SetPageIcon(response *applications.HTTPResponse, Icon string) {
+	response.Headers["x-cube-icon"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{Icon}}
 }
 
 //SetPageFID set the FID for the entity being shown on the page
-func SetPageFID(response *platform.HTTPResponse, FID string) {
-	response.Headers["x-cube-page-fid"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{FID}}
+func SetPageFID(response *applications.HTTPResponse, FID string) {
+	response.Headers["x-cube-page-fid"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{FID}}
 }
 
 // PageIntergrationType
@@ -51,11 +51,11 @@ const (
 )
 
 //SetPageIcon set the icon url/code on the response
-func SetPageIntegrations(response *platform.HTTPResponse, IntegrationType PageIntergrationType) {
-	response.Headers["x-cube-integrations"] = &platform.HTTPResponse_HTTPHeaderParameter{Values: []string{string(IntegrationType)}}
+func SetPageIntegrations(response *applications.HTTPResponse, IntegrationType PageIntergrationType) {
+	response.Headers["x-cube-integrations"] = &applications.HTTPResponse_HTTPHeaderParameter{Values: []string{string(IntegrationType)}}
 }
 
-func GetUrl(request *platform.HTTPRequest) *url.URL {
+func GetUrl(request *applications.HTTPRequest) *url.URL {
 	return &url.URL{
 		Scheme:     "https",
 		Host:       "apps.cubex.io",
